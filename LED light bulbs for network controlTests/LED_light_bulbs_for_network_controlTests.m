981a9351bed7940491ef4dafc6634ff7f0d89172
shaocong_ye <shaocong_ye@hotmail.com>
2014-09-02 11:04:13 +0800

    Initial Commit

---
+++ LED light bulbs for network controlTests/LED_light_bulbs_for_network_controlTests.m
@@ -0,0 +1,34 @@
+//
+//  LED_light_bulbs_for_network_controlTests.m
+//  LED light bulbs for network controlTests
+//
+//  Created by mac book on 14-9-2.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LED_light_bulbs_for_network_controlTests : XCTestCase
+
+@end
+
+@implementation LED_light_bulbs_for_network_controlTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
